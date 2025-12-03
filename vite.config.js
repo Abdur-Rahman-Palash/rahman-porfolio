0b729cced3f8c9eeb33c4d794fb38d293bf9f247
Md Abdur Rahman Palash <abdurrahmanpalashbd@gmail.com>
2025-12-03 10:29:29 +0600

    fixed

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rahman-porfolio/', // Must match repo name EXACTLY
-  build: {
-    outDir: 'docs', // Correct if deploying from docs folder
-  },
+  base: '/rahman-porfolio/',
   plugins: [react()],
 })
